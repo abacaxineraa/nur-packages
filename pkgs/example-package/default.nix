ff535fb49166f90d0741a4563ae39cf989766b8d
Ana Torres <69595809+abacaxineraa@users.noreply.github.com>
2024-08-26 23:19:51 -0400

    openfortivpn

--- pkgs/example-package/default.nix
+++ pkgs/example-package/default.nix
@@ -1,9 +1,25 @@
-{ stdenv }:
+{ lib
+, stdenv
+, fetchFromGitHub
+}:
 
 stdenv.mkDerivation rec {
-  name = "example-package-${version}";
-  version = "1.0";
-  src = ./.;
-  buildPhase = "echo echo Hello World > example";
-  installPhase = "install -Dm755 example $out";
+  pname = "openfortivpn-webview";
+  version = "1.2.0-electron";
+
+  src = fetchFromGitHub {
+    owner = "gm-vm";
+    repo = "openfortivpn-webview";
+    rev = "v${version}";
+    hash = "sha256-HheqDjlWxHJS0+OEhRTwANs9dyz3lhhCmWh+YH4itOk=";
+  };
+
+  meta = with lib; {
+    description = "";
+    homepage = "https://github.com/gm-vm/openfortivpn-webview";
+    license = licenses.mit;
+    maintainers = with maintainers; [ "abacaxineraa" ];
+    mainProgram = "openfortivpn-webview";
+    platforms = platforms.all;
+  };
 }
